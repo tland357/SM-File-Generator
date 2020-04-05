892f9bceaf8ca96e120ed73da85992037410476c
tland357 <ikamon357@gmail.com>
2020-04-05 00:29:04 -0400

    Cleaned up files

--- APPMapper.cpp
+++
@@ -1,8 +0,0 @@
-#include "APPMapper.hpp"
-#include "WINMapper.hpp"
-
-bool APPMapper::OnInit() {
-	static WINMapper* window = new WINMapper(nullptr);
-	window->Show();
-	return true;
-}
--- APPMapper.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <wx/app.h>
-class APPMapper : public wxApp
-{
-public:
-	virtual bool OnInit() override;
-};
-
